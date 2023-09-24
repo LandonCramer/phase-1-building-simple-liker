ac377cd11638334e552609ec42781d4259aa49d9
Landon <landoncramer1@gmail.com>
2023-09-24 16:41:21 -0700

    building simpler liker

--- main.js
+++ main.js
@@ -4,9 +4,33 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const errorModel = document.getElementById('modal')
+errorModel.classList.add('hidden')
+
+const heartClicked = (event) => {
+  mimicServerCall()
+  .then(() => {
+    if(event.target.textContent === EMPTY_HEART) {
+      event.target.textContent = FULL_HEART
+      event.target.classList.add('activated-heart')
+    } else {
+      event.target.textContent = EMPTY_HEART
+      event.target.classList.remove('activated-heart')
+    }
+  })
+  .catch(() => {
+    errorModel.classList.remove('hidden')
+    setTimeout(function() {
+      errorModel.classList.add('hidden')
+    }, 3000)
+  })
+}
 
 
-
+let hearts = document.getElementsByClassName('like-glyph')
+for(let heart of hearts) {
+  heart.addEventListener('click', heartClicked)
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
